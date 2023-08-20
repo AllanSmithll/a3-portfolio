00795ea963d0d939fc818365dfaa45d7fa28496c
allansmithll <allanammancio2004@gmail.com>
2023-08-19 23:18:18 -0300

    refatorando portfolio

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Olá pessoas, sou o <span className="purple">Allan Amancio </span>
+            da <span className="purple"> Paraíba, Brasil.</span>
+            <br /> Eu estou no penúltimo ano do curso Sistemas para Internet no Instituto Federal da Paraíba (IFPB).
+            <br />
+            Atualmente, estou a procura de alguma vaga de estágio na área de Tecnologia da Informação.
+            <br />
+            <br />
+            Abaixo, algumas atividades que gosto de fazer no meu dia a dia!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Jogar videogames
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Praticar esportes
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Ler bastante
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Faça a diferença na vida de alguém"{" "}
+          </p>
+          <footer className="blockquote-footer">Ninguém em específico hahaha</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
---
+++ src/components/Home/Home2.js
@@ -0,0 +1,122 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              DEIXE-ME ME <span className="purple"> APRESENTAR </span>
+            </h1>
+            <p className="home-about-body">
+              Sou alguém apaixonado pela área de Engenharia de Software.
+              <br />
+              <br />Eu sou fluente em várias linguagens de programação, como
+              <i>
+                <b className="purple"> Python, Javascript/TypeScript, Java and C#. </b>
+              </i>
+              <br />
+              <br />
+              Minha área de interesse, como dito anteriormente, é a de &nbsp;
+              <i>
+                <b className="purple">Engenharia de Software. </b> Gosto de parte de {" "}
+                <b className="purple"> desenvolvimento de softwares, análise de sistemas, banco de dados e pesquisar sobre coisas relacionadas à área.
+                </b>
+              </i>
+              <br />
+              <br />
+              Constantemente, eu também aplico minha paixão por desenvolver produtos Web com <b className="purple">Node.js</b> e
+              <i>
+                <b className="purple">
+                  {" "}
+                  Bibliotecas e Frameworks Modernos de JavaScript e TypeScript,
+                </b>
+              </i>
+              &nbsp; como
+              <i>
+                <b className="purple"> React.js e Angular.</b>
+              </i>
+              <br />
+              <br />
+              Além disso, não posso deixar de ressaltar que uso as linguagens <i>
+                <b className="purple">Java e C#
+                </b>
+                </ i>
+                &nbsp; para fazer aplicações bem mais robustas, junto de um bom framework, como o próprio 
+                 <i>
+                <b className="purple">                Angular.
+                </b>
+                </ i> 
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>ENCONTRE-ME EM</h1>
+            <p>
+              Fique à vontade para <span className="purple"> me seguir ou conectar </span>
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/AllanSmithll"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://twitter.com/AllanSmithll2"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/allan-alves-amancio-211632197/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/allan_smith_pb/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
